088e4b679dc5a051d164b678b7f341445c32f1fb
dimas4ek <77115571+dimas4ek@users.noreply.github.com>
2022-05-20 16:56:55 +0500

    Create Boss.cs

---
+++ Codes/Boss.cs
@@ -0,0 +1,15 @@
+namespace TEXT_GAME
+{
+    class Boss
+    {
+        public string Name;
+
+        public int Attack = 0;
+        public int Health = 0;
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+    }
+}
