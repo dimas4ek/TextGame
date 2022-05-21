e7b381501b5a9c9dddc87dfa5714767f10cfc970
dimas4ek <77115571+dimas4ek@users.noreply.github.com>
2022-05-21 11:20:40 +0500

    Add files via upload

---
+++ Game/Codes/Boss.cs
@@ -0,0 +1,10 @@
+﻿namespace TEXT_GAME
+{
+    class Boss
+    {
+        public string Name;
+
+        public int Attack = 0;
+        public int Health = 0;
+    }
+}
---
+++ Game/Codes/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace TEXT_GAME
+{
+    class Player
+    {
+        public int Attack = 30;
+        public int Health = 200;
+    }
+}
